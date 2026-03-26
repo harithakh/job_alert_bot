bf665352e48207bd98328e990bc429fcb455687c
haritha <hkhulangamuwa@gmail.com>
2026-03-26 13:30:43 +0530

    initial commit: scraper_site_1 and app

---
+++ bot.py
@@ -0,0 +1,42 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+def send_job_alert(job):
+    text = f"""
+    <b>New Job Alert</b>
+
+    <b>Title:</b> {job['title']}
+    <b>Company:</b> {job['company']}
+    <b>Location:</b> {job['location']}
+    <b>Posted:</b> {job['posted_date']} | {job['posted_time']}
+
+    <a href="{job['link']}">Link</a>
+    """
+
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+    response = requests.post(url, data={
+        "chat_id": CHAT_ID,
+        "text":text,
+        "parse_mode": "HTML"
+        })
+    return response.ok
+
+# if __name__ == "__main__":
+#     print("TOKEN:", BOT_TOKEN)
+#     print("CHAT_ID:", CHAT_ID)
+#     test = send_job_alert({
+#         "title": "Test Job",
+#         "company": "Test Company",
+#         "location": "Remote",
+#         "posted_date": "2026-03-26",
+#         "posted_time": "04:27:23",
+#         "link": "https://example.com"
+#     })
+#     print("Message sent:", test)
---
+++ scaper_site_1.py
@@ -0,0 +1,55 @@
+import requests
+from bs4 import BeautifulSoup
+
+# print(response.status_code)
+
+def scrape_jobs_site_1():
+
+    url = "https://itpro.lk/"
+    response = requests.get(url)
+
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    all_jobs_first_page = soup.find_all("article")
+
+    jobs = []
+
+    for article in all_jobs_first_page:
+
+        # job link
+        link_tag = article.select_one("a.job-record-link")
+        job_link = link_tag["href"] if link_tag else None
+
+        #job title
+        title = article.select_one("span.job-title")
+        job_title = title.get_text(strip=True) if title else None
+
+        # company name
+        company = article.select_one("span.jc-company")
+        company_name = company.get_text(strip=True) if company else None
+
+        # location
+        location_span = article.select_one("div.job-record-location span.la")
+        if location_span:
+            #  Remove the img tag, then grab the remaining text
+            img = location_span.find("img")
+            if img:
+                img.decompose()
+            location = location_span.get_text(strip=True)
+        else:
+            location = None
+
+        # date and time
+        time_tag = article.select_one("time.time-posted")
+        posted_date = time_tag["datetime"].split("T")[0] if time_tag else None
+        posted_time = time_tag["datetime"].split("T")[1] if time_tag else None
+
+        jobs.append({
+            "title": job_title,
+            "company": company_name,
+            "location": location,
+            "link": job_link,
+            "posted_date": posted_date,
+            "posted_time": posted_time,})
+
+    return jobs
